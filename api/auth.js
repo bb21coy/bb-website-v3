84768f88242e5292fe9df30c64b5c1dcf609f9ef
Dylan <dylanyeowf@gmail.com>
2025-07-11 12:21:53 +0800

    test

--- api/auth.js
+++ api/auth.js
@@ -1,16 +1,5 @@
-export default function handler(req, res) {
-    try {
-        const { method } = req;
-        const url = req.url;
-        const routeKey = `${method} ${url}`;
+// /api/hello.js
 
-        switch (routeKey) {
-            case "GET /api/auth/login":
-                return res.status(200).json({ name: 'John Doe' })
-            default:
-                return res.status(404).json({ message: 'Route not found' })
-        }
-    } catch (error) {
-        return res.status(500).json({ message: error.message || error })
-    }
+export default function handler(req, res) {
+    res.status(200).json({ message: 'Hello, world!' });
 }
