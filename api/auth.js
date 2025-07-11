5088a5c6eecd1a176971f385e81051960e763268
Dylan <dylanyeowf@gmail.com>
2025-07-11 22:07:06 +0800

    updated cors

--- api/auth.js
+++ api/auth.js
@@ -34,6 +34,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     if (req.method === 'OPTIONS') return res.status(200).end();
 
