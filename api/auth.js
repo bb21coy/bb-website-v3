d8b077926ff9f776b42bec0058f0d0f3c74c7a2b
Dylan <dylanyeowf@gmail.com>
2025-07-11 12:11:38 +0800

    added error handling

--- api/auth.js
+++ api/auth.js
@@ -1,12 +1,16 @@
 export default handler = (req, res) => {
-    const { method } = req;
-    const url = req.url;
-    const routeKey = `${method} ${url}`;
+    try {
+        const { method } = req;
+        const url = req.url;
+        const routeKey = `${method} ${url}`;
 
-    switch (routeKey) {
-        case "GET /api/auth/login":
-            return res.status(200).json({ name: 'John Doe' })
-        default:
-            return res.status(404).json({ message: 'Route not found' })
+        switch (routeKey) {
+            case "GET /api/auth/login":
+                return res.status(200).json({ name: 'John Doe' })
+            default:
+                return res.status(404).json({ message: 'Route not found' })
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
     }
 }
