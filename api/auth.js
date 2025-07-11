e4fb4492de8cf259b4fb69f72d7ba133f3b027c6
Dylan <dylanyeowf@gmail.com>
2025-07-11 22:01:39 +0800

    updated cors

--- api/auth.js
+++ api/auth.js
@@ -32,8 +32,10 @@ const checkAuthorization = (tokenType, res, allowedType = ["Admin", "Officer", "
 
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') return res.status(200).end();
 
     try {
         const route = req.headers['x-route']
