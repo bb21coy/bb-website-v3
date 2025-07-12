edf1a6190ea46bafef5303054da6d34e1e8e8ee8
Dylan <dylanyeowf@gmail.com>
2025-07-12 21:18:44 +0800

    updated headers

--- api/auth.js
+++ api/auth.js
@@ -34,7 +34,7 @@ module.exports = async (req, res) => {
     const origin = req.headers.origin || '*';
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-route');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     if (req.method === 'OPTIONS') {
